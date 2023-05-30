acaf62b17832bb3dd79d6a6af6b14f068445768e
Andra_Storm <joaopaulogomes04@hotmail.com>
2023-05-30 12:28:15 -0400

    adições de img

--- resources/views/empresas/addempresas.blade.php
+++ resources/views/empresas/addempresas.blade.php
@@ -1,88 +1,65 @@
 @extends('layouts.app')
 @section('conteudo')
 
-<section>
-
-    <div>
-        <img src="" alt="">
+    <div class="textlogin text-center">
+      <h4>Encontre profissionais qualificados para trabalhar em seu negócio!</h4>
     </div>
 
-    <div class="formempresas">
+<section class="d-flex py-5">
+    <div class="ms-5 ps-5">
+        <img src="/assets/images/vectors/tech company-cuate.png" alt="iamgem ilustrativa" width="600" height="600">
+    </div>
+
+    <div class="formempresas d-flex">
         <form method="post" action="{{route('salvarempresas')}}"> <!-- Essas rotas ficam na api.php -->
             @csrf
-            <label for="nome">Nome da Empresa:</label>
-            <input type="text" id="nome" name="nome" required><br><br>
+            <div class="d-flex">
+              <div class="mx-4">
+                <div class="mb-3">
+                  <label for="formGroupExampleInput" class="form-label">Nome da Empresa:</label>
+                  <input class="form-control" type="text" id="nome" name="nome" required>
+                </div>
 
-            <label for="descricao">Descrição da Empresa:</label>
-            <input type="text" id="descricao" name="descricao" required><br><br>
+                <div class="mb-3">
+                  <label for="formGroupExampleInput" class="form-label">Email Corporativo da empresa:</label>
+                  <input class="form-control" type="email" id="email" name="email" required>
+                </div>
+            
+                <div class="mb-3">
+                  <label for="formGroupExampleInput" class="form-label">Senha:</label>
+                  <input class="form-control" type="text" id="senha" name="senha" required>
+                </div>
+              </div>
 
-            <label for="email">Email Corporativo da empresa:</label>
-            <input type="text" id="email" name="email" required><br><br>
+              <div class="mx-2">
+                <div class="mb-3">
+                  <label for="formGroupExampleInput" class="form-label">Telefone:</label>
+                  <input class="form-control" type="text" id="telefone" name="telefone" required>
+                </div>
 
-            <label for="senha">Senha:</label>
-            <input type="text" id="senha" name="senha" required><br><br>
+                <div class="mb-3">
+                  <label for="formGroupExampleInput" class="form-label">CNPJ:</label>
+                  <input class="form-control" type="text" id="cnpj" name="cnpj" required>
+                </div>
 
-            <label for="telefone">Telefone:</label>
-            <input type="text" id="telefone" name="telefone" required><br><br>
+                <div class="mb-3">
+                  <label for="formFile" class="form-label">Escolha a Imagem Principal de sua Empresa:</label>
+                  <input class="form-control" type="file" id="logo" name="logo">
+                </div>
+              </div>
+            </div>
 
-            <label for="cnpj">CNPJ:</label>
-            <input type="text" id="cnpj" name="cnpj" required><br><br>
+            <div class="d-flex justify-content-center">
+              <div class="mb-3">
+                <label for="formGroupExampleInput" class="form-label">Descrição da Empresa:</label>
+                <textarea class="form-control exampleFormControlTextarea1" id="descricao" rows="3" name="descricao" required></textarea>
+              </div>
+            </div>
 
-            <label for="cpf">CPF:</label>
-            <input type="text" id="cpf" name="cpf" required><br><br>
-
-            <label for="logo">Logo:</label>
-            <input type="text" id="logo" name="logo" required><br><br>
-
-            <button type="submit">Submit</button>
+            <div class="d-flex justify-content-center my-3">
+              <button type="submit" class="btn btn-lg text-white" style="background-color:#546E7A" >Cadastrar-me</button>
+            </div>
         </form>
     </div>
 </section>
-
-<div class="d-flex justify-content-center">
-    <form class="container bg-danger row g-3 my-5">
-  <div class="col-md-6">
-    <label for="inputEmail4" class="form-label">Email</label>
-    <input type="email" class="form-control" id="inputEmail4">
-  </div>
-  <div class="col-md-6">
-    <label for="inputPassword4" class="form-label">Password</label>
-    <input type="password" class="form-control" id="inputPassword4">
-  </div>
-  <div class="col-12">
-    <label for="inputAddress" class="form-label">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-  </div>
-  <div class="col-12">
-    <label for="inputAddress2" class="form-label">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-  </div>
-  <div class="col-md-6">
-    <label for="inputCity" class="form-label">City</label>
-    <input type="text" class="form-control" id="inputCity">
-  </div>
-  <div class="col-md-4">
-    <label for="inputState" class="form-label">State</label>
-    <select id="inputState" class="form-select">
-      <option selected>Choose...</option>
-      <option>...</option>
-    </select>
-  </div>
-  <div class="col-md-2">
-    <label for="inputZip" class="form-label">Zip</label>
-    <input type="text" class="form-control" id="inputZip">
-  </div>
-  <div class="col-12">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <div class="col-12">
-    <button type="submit" class="btn btn-primary">Sign in</button>
-  </div>
-    </form>
-</div>
 @endsection
